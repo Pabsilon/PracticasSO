bb74862e7bff964cf552f89994a4633269c24167
Marina <galleria_22@hotmail.com>
2016-01-10 14:25:36 +0100

    Script done. There are some errors for 3 or more CPUS
    Using the original Makefile with -DPOSIX_BARRIER works fine.

--- Practica 3/schedsim/Script.sh
+++ Practica 3/schedsim/Script.sh
@@ -4,15 +4,16 @@ SCHEDFILE="What file do you want to execute?"
 FNREG="The specified file $FILE isn't regular."
 FNEXIST="The specified file $FILE doesn't exist."
 SCHEDCPUNUM="How many CPUs do you want to simulate? 1-8."
+INVALIDCPUNUM="Invalid number of CPUS, it must be between 1-8."
 
-FOUND=0
-while [ $FOUND -ne 1 ] ; do
+END=0
+while [ $END -ne 1 ] ; do
 	echo $SCHEDFILE
 	read FILE
 
 	if [ -e $FILE ] ; then
 		if [ -r $FILE ] ; then
-			let FOUND=1
+			let END=1
 		else
 			echo $FNREG
 		fi
@@ -20,32 +21,41 @@ while [ $FOUND -ne 1 ] ; do
 		echo $FNEXIST
 	fi
 done
+
 CPUNUM=0
-echo $SCHEDCPUNUM
-read CPUNUM
-	if [ $CPUNUM -le 0 ] ; then
-		let CPUNUM = 1
-	fi
-	if [ $CPUNUM -ge 9 ] ; then
-		let CPUNUM = 8
-	fi
-echo $CPUNUM
+END1=0
+while [ $END1 -ne 1 ] ; do
+    echo $SCHEDCPUNUM
+    read CPUNUM
+    if [ $CPUNUM -le 0 ] || [ $CPUNUM -ge 9 ] ; then
+	    echo $INVALIDCPUNUM
+    else 
+        let END1=1
+    fi
+done
 
 
+if [ ! -d "resultados" ]
+then
+	mkdir resultados
+fi
 
-
-mkdir resultados
-
-for SCHEDNAME in 'seq RR SJF FCFS PRIO' do
-	for (CPUS=1; $CPUS<=$CPUNUM; CPUS++;) do
+for SCHEDNAME in RR SJF FCFS PRIO
+do
+	for ((CPUS=1; $CPUS<=$CPUNUM; CPUS++))
+    do
 		./schedsim -i $FILE -n $CPUS -s $SCHEDNAME
-			for (i=1; $i<=$CPUNUM i++) do
-				mv CPU_$i.log resultados/$SCHEDNAME-CPU_$i.log
-			done
-		cd ../gantt-plot
-		for (i=1; $i<=$CPUNUM; i++) do
-			./generate_gantt_chart ../schedsim/resultados/$SCHEDNAME-CPU_$i.log
-			mv CPU_$i.eps ../schedsim/resultados/DIAGANTT-$SCHEDNAME-CPU_$i.eps 
+		for ((i=1; $i<=$CPUS; i++))
+        do
+            let "NUM=i-1"
+			mv CPU_$NUM.log resultados/$SCHEDNAME-CPU_$NUM.log
+		done
+		cd ../gantt-gplot
+		for ((i=1; $i<=$CPUS; i++))
+        do
+            let "NUM_1=i-1"
+			./generate_gantt_chart ../schedsim/resultados/$SCHEDNAME-CPU_$NUM_1.log
+			mv ../schedsim/resultados/$SCHEDNAME-CPU_$NUM_1.eps ../schedsim/resultados/DIAGANTT-$SCHEDNAME-CPU_$NUM_1.eps 
 		done
 		cd ../schedsim
 	done
